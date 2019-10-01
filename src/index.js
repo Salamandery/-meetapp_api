1bd93e03f8b69f6557458cbfb45b09a553cdd9fd
salamandery <sp.ront@hotmail.com>
2019-10-01 06:58:57 -0300

    padronização / estilização dos códigos

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ import routes from './routes';
 
 // Classe do servidor onde ocorre toda a configuração de middlewares e rotas
 class App {
-    constructor(){
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    // Instanciando servidor express
+    this.server = express();
+    // Chamando métodos
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        // Requisições do tipo json
-        this.server.use(express.json());
-        // Variaveis de ambiente
-        env.config();
-    }
+  middlewares() {
+    // Requisições do tipo json
+    this.server.use(express.json());
+    // Variaveis de ambiente
+    env.config();
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    // Configurando rotas na aplicação
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
